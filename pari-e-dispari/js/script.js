6ed11f5fee47be34925f8045d333e14a82646d68
manuelmenama <manuel.mena090589@gmail.com>
2022-10-19 19:22:36 +0200

    ended pari dispari

--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -7,3 +7,48 @@ console.log('"Pari e dispari" loaded');
 //se numero generato è come dice l'utente: utente vince
 //altrimenti utente perde
 
+let playButton = document.querySelector('button');
+
+playButton.addEventListener('click', function(){
+
+  const pariDispari = document.getElementById('pod').value;
+  let numberSelected = document.getElementById('number').value;
+  const textParagraph = document.querySelector('p');
+
+  let control;
+  console.log(numberSelected);
+  console.log(pariDispari);
+  
+  let browserNumber = randomNumberGenerator(1, 5);
+  console.log(browserNumber);
+  
+  let numberSum = parseInt(numberSelected) + browserNumber;
+  
+  console.log(numberSum);
+  
+  if(!(numberSum % 2)) control = "pari";
+  else control = "dispari";
+  
+  
+  console.log(control);
+  
+  if(pariDispari == control){
+    textParagraph.innerHTML = `
+      L'utente gioca <strong>${numberSelected}</strong> e <strong>${pariDispari}</strong>. Il browser gioca <strong>${browserNumber}</strong> La somma è <strong>${numberSum}</strong>. L'utente vince!
+    `;
+    console.log('Utente vince!');
+  }else{
+    textParagraph.innerHTML = `
+      L'utente gioca <strong>${numberSelected}</strong> e <strong>${pariDispari}</strong>. Il browser gioca <strong>${browserNumber}</strong> La somma è <strong>${numberSum}</strong>. L'utente perde...
+    `;
+    console.log('Utente perde...');
+  }
+
+});
+
+
+
+function randomNumberGenerator(min, max){
+  const numberGenerated = Math.floor(Math.random() * max - min + 1) + min;
+  return numberGenerated;
+}
