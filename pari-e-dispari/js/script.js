d4e18645d38698bc18dc01902d64dc4f2c3cc4c1
manuelmenama <manuel.mena090589@gmail.com>
2022-10-19 17:46:58 +0200

    started pari e dispari

--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -1,1 +1,9 @@
-console.log('Ciao world!');
+console.log('"Pari e dispari" loaded');
+
+//salviamo in variabili gli imput dell'utente: pari o dispari + numero da 1 a 5
+//generiamo un numero random da 1 a 5 con una funzione
+  //**facoltativo**generiamo numero da 1 a 2 per il browser 1 = dispari 2 = pari**
+//facciamo la somma del numero dell'utente con numero generato
+//se numero generato è come dice l'utente: utente vince
+//altrimenti utente perde
+
