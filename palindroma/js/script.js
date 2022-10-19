d4e83d1a011d8fc7a11a63d0aa7460cee5522820
manuelmenama <manuel.mena090589@gmail.com>
2022-10-19 16:45:07 +0200

    [add] logic

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,1 +1,25 @@
 console.log('Loaded "Palindroma"');
+
+//chiedo la parola all'utente
+//inserisco la stringa in un altra stringa ma partendo dall'ultima lettera
+//confronto le due parole
+//se uguali allora è palindroma
+
+palindromaCheck();
+
+function palindromaCheck(){
+  const askedWord = "anna";
+  let reverseWord = "";
+  let counterString = askedWord.length - 1;
+  
+  while(counterString >= 0){    
+    reverseWord += askedWord[counterString];
+    counterString--;
+  }
+
+  if(askedWord === reverseWord){
+    console.log(askedWord, 'Parola palindroma!');
+  }else{
+    console.log(askedWord, 'Non è palindroma...');
+  }
+}
